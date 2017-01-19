53614366cc9583c6f5f7e809964bc1e3010cb242
RaghebAD <ragheb.abo.dargham@gmail.com>
2017-01-19 13:52:43 +0200

    bug fix

--- views/sections/home/controller.js
+++ views/sections/home/controller.js
@@ -85,7 +85,7 @@ overApp.controller('overviewCtrl', ['$scope', 'ngDataApi', '$timeout', function 
 				"deployDriver": "container.docker.remote",
 				"deployType": "container"
 			};
-			output.deployAnalytics = $scope.analytics.local;
+			output.deployAnalytics = $scope.analytics.remote;
 		}
 		//if docker remote is selected
 		else if (!$scope.manual && !$scope.local && $scope.remote && !$scope.docker && $scope.kubernetes) {
@@ -93,7 +93,7 @@ overApp.controller('overviewCtrl', ['$scope', 'ngDataApi', '$timeout', function 
 				"deployDriver": "container.kubernetes.remote",
 				"deployType": "container"
 			};
-			output.deployAnalytics = $scope.analytics.local;
+			output.deployAnalytics = $scope.analytics.remote;
 		}
 		
 		var options = {
